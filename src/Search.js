d66bf3a258cf583f7810f57532a81eb6ac7d85cb
wtface <nkhpark@hotmail.com>
2017-12-19 22:12:29 +0800

    feat: add link to / in search bar

--- src/Search.js
+++ src/Search.js
@@ -31,7 +31,10 @@ class Search extends Component{
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <Link
+            to='/'
+            className='close-search'
+          ></Link>
           <div className="search-books-input-wrapper">
             <input 
               type="text" 
@@ -44,7 +47,26 @@ class Search extends Component{
 
         <div className="search-books-results">
           <ol className="books-grid">
-          
+          {showingBooks.map((book) => ( 
+          	<li>
+              <div className="book">
+                <div className="book-top">
+                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url({book.imageLinks.thumbnail})' }}></div>
+                  <div className="book-shelf-changer">
+                    <select>
+                      <option value="none" disabled>Move to...</option>
+                      <option value="currentlyReading">Currently Reading</option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors}</div>
+              </div>
+            </li>
+          ))}
           </ol>
         </div>
       </div>
