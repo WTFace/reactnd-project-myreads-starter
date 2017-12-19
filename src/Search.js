ba90066671c89c96ce4b111f17e5cb349ca1f612
wtface <nkhpark@hotmail.com>
2017-12-20 00:23:25 +0800

    fix: change book property from 'name' to 'title'

--- src/Search.js
+++ src/Search.js
@@ -19,10 +19,11 @@ class Search extends Component{
     const { books } = this.props
     const { query } = this.state
     console.log(books)
+
     let showingBooks
     if (query) {
       const match = new RegExp(escapeRegExp(query), 'i')
-      showingBooks = books.filter((book) => match.test(book.name))
+      showingBooks = books.filter((book) => match.test(book.title))
     } else {
       showingBooks = []
     }
@@ -48,10 +49,11 @@ class Search extends Component{
         <div className="search-books-results">
           <ol className="books-grid">
           {showingBooks.map((book) => ( 
-          	<li>
+          	
+          	<li key={book.id}>
               <div className="book">
                 <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url({book.imageLinks.thumbnail})' }}></div>
+                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                   <div className="book-shelf-changer">
                     <select>
                       <option value="none" disabled>Move to...</option>
