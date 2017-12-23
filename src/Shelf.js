acc751269bae1a8184f256c58d8729c1440dfde7
wtface <nkhpark@hotmail.com>
2017-12-23 10:26:55 +0800

    feat: add Shelf component to list books

---
+++ src/Shelf.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import * as BooksAPI from './BooksAPI'
+
+class Shelf extends Component{
+	static propTypes = {
+    books: PropTypes.array,
+    onUpdate: PropTypes.func
+	}
+	updateShelf(book,shelf){
+    	BooksAPI.update(book,shelf)
+  	}
+
+	render(){
+		const { books, onUpdate } = this.props
+	    console.log(books)
+
+		return(
+			<div className="bookshelf-books">
+                <ol className="books-grid">
+                	{books.map((book) => ( 
+      	
+			          	<li key={book.id}>
+			              <div className="book">
+			                <div className="book-top">
+			                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+			                  <div className="book-shelf-changer">
+			                    <select onChange={(event) => onUpdate(book, event.target.value)}>
+			                      <option value="none" disabled>Move to...</option>
+			                      <option value="currentlyReading">Currently Reading</option>
+			                      <option value="wantToRead">Want to Read</option>
+			                      <option value="read">Read</option>
+			                      <option value="none">None</option>
+			                    </select>
+			                  </div>
+			                </div>
+			                <div className="book-title">{book.title}</div>
+			                <div className="book-authors">{book.authors}</div>
+			              </div>
+			            </li>
+			        ))}
+                </ol>
+            </div>
+		)
+	}
+}
+
+export default Shelf
